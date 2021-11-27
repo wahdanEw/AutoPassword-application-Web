a739c5472ab30100b2e36a39dcc7d3b38a01c010
wahdanEw <85776760+wahdanEw@users.noreply.github.com>
2021-11-27 20:39:02 +0200

    Update route.py

--- route.py
+++ route.py
@@ -1,4 +1,3 @@
-from os import readlink
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,6 +10,20 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+# Create Errors pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#Invalid Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
